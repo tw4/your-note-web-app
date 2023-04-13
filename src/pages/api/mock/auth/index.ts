9048c44613a3bd9d56d3cd1e2e0a69ae96e01a0b
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-13 13:52:34 +0300

    add: jwt

--- src/pages/api/mock/auth/index.ts
+++ src/pages/api/mock/auth/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { User } from '@/types';
 import { v4 } from 'uuid';
 import { ZodUserValidationSchema } from '@/zod/ZodValidationSchema';
+import jwt from 'jsonwebtoken';
 
 const post = (req: NextApiRequest, res: NextApiResponse) => {
   const user: User = req.body;
@@ -15,8 +16,12 @@ const post = (req: NextApiRequest, res: NextApiResponse) => {
     user.id = v4();
     user.createdAt = new Date().toLocaleDateString();
     user.lastLogin = new Date().toLocaleDateString();
-    user.token = v4();
     if (ZodUserValidationSchema.safeParse(user).success) {
+      user.token = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.MOCK_JWT_SECRET!,
+        { expiresIn: '1d' }
+      );
       return res.status(200).json(user);
     } else {
       return res.status(400).json({ message: 'User is invalid' });
@@ -28,7 +33,12 @@ const get = (req: NextApiRequest, res: NextApiResponse) => {
   const headers = req.headers;
   if (headers['api-key'] === process.env.MOCK_API_KEY) {
     if (req.query.email === 'test' && req.query.password === '123456') {
-      return res.status(200).json({ id: '1', token: v4() });
+      const token: string = jwt.sign(
+        { id: '1', email: req.query.email },
+        process.env.MOCK_JWT_SECRET!,
+        { expiresIn: '24h' }
+      );
+      return res.status(200).json({ id: '1', token: token });
     } else {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
