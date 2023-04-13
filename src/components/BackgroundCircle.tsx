27c87b56b3f1bbde940a5629b8d01524d864f829
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-13 20:02:57 +0300

    add: component background circle

---
+++ src/components/BackgroundCircle.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material';
+
+const BackgroundCircle = () => {
+  return (
+    <Box
+      position="absolute"
+      height="50vh"
+      width="100vh"
+      borderRadius="100%"
+      sx={{
+        backgroundColor: '#3A3AF4',
+        boxShadow: '0 0 150px 130px #3A3AF4 ',
+        top: '-40vh',
+        left: '25vw',
+        opacity: 0.4,
+      }}
+      zIndex="-1"
+    ></Box>
+  );
+};
+
+export default BackgroundCircle;
