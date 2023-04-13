32b7c4a637a4237b852263593e43c443622052d5
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-13 21:27:40 +0300

    add: footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from '@mui/material';
+const Footer = () => {
+  return (
+    <Box position="fixed" bottom="1vh" left="1vw">
+      <Typography>Pre-Alpha</Typography>
+    </Box>
+  );
+};
+
+export default Footer;
