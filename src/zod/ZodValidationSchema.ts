f2e4b3174cc0e0598ea82df7b18790ce1d782e2b
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-14 11:25:55 +0300

    add: zod user register validation schema

--- src/zod/ZodValidationSchema.ts
+++ src/zod/ZodValidationSchema.ts
@@ -10,3 +10,9 @@ export const ZodUserValidationSchema = z.object({
   lastLogin: z.string().min(1),
   notes: z.string().min(2),
 });
+
+export const ZodUserRegisterValidationSchema = z.object({
+  name: z.string().min(1).max(100),
+  email: z.string().email().min(1).max(100),
+  password: z.string().min(6).max(20),
+});
