ca82d7c43e02c09c3542677e32874e44803fa962
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-14 14:30:46 +0300

    fix: firebase export

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -5,7 +5,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,7 +19,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const auth = getAuth(app);
+initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
