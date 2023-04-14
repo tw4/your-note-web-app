4e1b96253cea8626e3c29c092be73ed8d571e64e
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-14 15:10:02 +0300

    fix: typo

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,43 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { User } from '@/types';
-import { ZodUserRegisterValidationSchema } from '@/zod/ZodValidationSchema';
-import { createUserWithEmailAndPassword } from '@firebase/auth';
-import { auth, db } from '@/services/Firebase';
-import { addDoc, collection } from '@firebase/firestore';
+import { auth } from '@/services/Firebase';
+import { signInWithEmailAndPassword } from '@firebase/auth';
 
 const post = (req: NextApiRequest, res: NextApiResponse) => {
   const headers = req.headers;
   if (headers['api-key'] !== process.env.API_KEY) {
     return res.status(401).json({ message: 'Unauthorized' });
   } else {
-    const user: User = req.body;
-    if (user && ZodUserRegisterValidationSchema.safeParse(user).success) {
-      createUserWithEmailAndPassword(auth, user.email, user.password)
-        .then(async response => {
-          const token = await response.user.getIdToken();
-          addDoc(collection(db, 'users'), {
-            id: response.user.uid,
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            token: token,
-            createdAt: new Date().toISOString(),
-            lastLogin: new Date().toISOString(),
-            notes: [],
-          }).catch(err => {
-            return res.status(400).json({ message: err.message });
-          });
-          return res.status(200).json({
-            userID: response.user.uid,
-            token: token,
-          });
-        })
-        .catch(err => {
-          return res.status(400).json({ message: err.message });
+    signInWithEmailAndPassword(auth, req.body.email, req.body.password)
+      .then(async response => {
+        const token = await response.user.getIdToken();
+        return res.status(200).json({
+          userID: response.user.uid,
+          token: token,
+          verified: response.user.emailVerified,
         });
-    } else {
-      return res.status(400).json({ message: 'Bad Request' });
-    }
+      })
+      .catch(err => {
+        return res.status(400).json({ message: err.message });
+      });
   }
 };
 
--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,29 +1,54 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { auth } from '@/services/Firebase';
-import { signInWithEmailAndPassword } from '@firebase/auth';
+import { User } from '@/types';
+import { ZodUserRegisterValidationSchema } from '@/zod/ZodValidationSchema';
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from '@firebase/auth';
+import { auth, db } from '@/services/Firebase';
+import { addDoc, collection } from '@firebase/firestore';
 
-const get = (req: NextApiRequest, res: NextApiResponse) => {
+const post = (req: NextApiRequest, res: NextApiResponse) => {
   const headers = req.headers;
   if (headers['api-key'] !== process.env.API_KEY) {
     return res.status(401).json({ message: 'Unauthorized' });
   } else {
-    signInWithEmailAndPassword(auth, req.body.email, req.body.password)
-      .then(async response => {
-        const token = await response.user.getIdToken();
-        return res.status(200).json({
-          userID: response.user.uid,
-          token: token,
+    const user: User = req.body;
+    if (user && ZodUserRegisterValidationSchema.safeParse(user).success) {
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then(async response => {
+          await sendEmailVerification(response.user);
+          const token = await response.user.getIdToken();
+          addDoc(collection(db, 'users'), {
+            id: response.user.uid,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            token: token,
+            createdAt: new Date().toISOString(),
+            lastLogin: new Date().toISOString(),
+            notes: [],
+          }).catch(err => {
+            return res.status(400).json({ message: err.message });
+          });
+          return res.status(200).json({
+            userID: response.user.uid,
+            token: token,
+            verified: response.user.emailVerified,
+          });
+        })
+        .catch(err => {
+          return res.status(400).json({ message: err.message });
         });
-      })
-      .catch(err => {
-        return res.status(400).json({ message: err.message });
-      });
+    } else {
+      return res.status(400).json({ message: 'Bad Request' });
+    }
   }
 };
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    get(req, res);
+    post(req, res);
   } else {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
