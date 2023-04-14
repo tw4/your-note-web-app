21e03aaecf1be41f3815b52e27c352b4058df326
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-14 11:26:07 +0300

    add: Firebase services

---
+++ src/services/Firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY_FIREBASE,
+  authDomain: process.env.AUTH_DOMAIN_FIREBASE,
+  projectId: process.env.PROJECT_ID_FIREBASE,
+  storageBucket: process.env.STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID_FIREBASE,
+  appId: process.env.APP_ID_FIREBASE,
+  measurementId: process.env.MEASUREMENT_ID_FIREBASE,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
