5ea168f9e6214bb756c49a4c5983f48e4fef6758
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-14 14:30:30 +0300

    update: zod user validation schema remove password

--- src/zod/ZodValidationSchema.ts
+++ src/zod/ZodValidationSchema.ts
@@ -4,7 +4,6 @@ export const ZodUserValidationSchema = z.object({
   id: z.string().min(1),
   name: z.string().min(1),
   email: z.string().email(),
-  password: z.string().min(6),
   token: z.string().min(1),
   createdAt: z.string().min(1),
   lastLogin: z.string().min(1),
