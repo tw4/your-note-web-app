a33b52e0dc04af65d1297002cbfdb01ed476487f
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-15 14:24:30 +0300

    update: next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MOCK_API_KEY: process.env.MOCK_API_KEY,
+    API_KEY: process.env.API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
