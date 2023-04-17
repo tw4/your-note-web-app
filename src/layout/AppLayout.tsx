bf5679a740a708cf9de2ab45b218e59e5ffc90be
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-17 22:09:53 +0300

    add: App Layout

---
+++ src/layout/AppLayout.tsx
@@ -0,0 +1,18 @@
+import { FC, ReactNode } from 'react';
+import { Box } from '@mui/material';
+import AppNavbar from '@/components/appNavbar/AppNavbar';
+
+type IProps = {
+  children?: ReactNode;
+};
+
+const AppLayout: FC<IProps> = ({ children }) => {
+  return (
+    <Box>
+      <AppNavbar />
+      <Box>{children}</Box>
+    </Box>
+  );
+};
+
+export default AppLayout;
