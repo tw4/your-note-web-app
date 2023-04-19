6fc73ca5ada6f01a8890ff2fce362433258cc26a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-19 23:47:45 +0300

    add: app side bar

--- src/components/app/AppSideBar.tsx
+++ src/components/app/AppSideBar.tsx
@@ -1,12 +1,46 @@
-import { Button, Stack, Typography } from '@mui/material';
+import {
+  Button,
+  ListItemIcon,
+  MenuItem,
+  MenuList,
+  Stack,
+  Typography,
+} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-const AppSideBar = () => {
+import { FC } from 'react';
+import { Note } from '@/types';
+import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
+
+type IProps = {
+  getNoteDetail: (note: Note) => void;
+  getSelectedCategory: (category: string) => void;
+  selectedCategory: string | null;
+  categoryList: string[];
+};
+
+const AppSideBar: FC<IProps> = ({
+  getNoteDetail,
+  getSelectedCategory,
+  selectedCategory,
+  categoryList,
+}) => {
+  const newNote: Note = {
+    id: '',
+    title: 'Title',
+    content: '',
+    createdBy: '',
+    createdAt: `${new Date().toLocaleDateString('en-GB')}`,
+    updatedAt: '',
+    category: 'Folder Name for note',
+  };
+
   return (
     <Stack direction="column" marginLeft="1.5%" marginRight="1.5%" width="20vw">
       <Typography textAlign="start" fontSize="x-large">
         Notes
       </Typography>
       <Button
+        onClick={() => getNoteDetail(newNote)}
         variant="contained"
         color="secondary"
         startIcon={<AddIcon />}
@@ -14,6 +48,55 @@ const AppSideBar = () => {
       >
         New Note
       </Button>
+      <Stack
+        direction="column"
+        marginTop="5%"
+        height="25vh"
+        sx={{
+          overflow: 'scroll',
+          overflowX: 'hidden',
+          '&::-webkit-scrollbar': {
+            width: '8px',
+            backgroundColor: 'background.paper',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            borderRadius: '8px',
+            backgroundColor: '#7f7f7f',
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#555',
+          },
+          '&::-webkit-scrollbar-track': {
+            borderRadius: '8px',
+            backgroundColor: 'secondary',
+          },
+        }}
+      >
+        <Stack direction="row" justifyContent="space-between">
+          <Typography color="grey">Folders</Typography>
+          <FolderOpenOutlinedIcon sx={{ color: 'grey' }} />
+        </Stack>
+        <MenuList>
+          {categoryList.map(category => {
+            return (
+              <MenuItem onClick={() => getSelectedCategory(category)}>
+                <ListItemIcon>
+                  <FolderOpenOutlinedIcon
+                    sx={{
+                      color: selectedCategory === category ? 'white' : 'grey',
+                    }}
+                  />
+                </ListItemIcon>
+                <Typography
+                  color={selectedCategory === category ? 'white' : 'grey'}
+                >
+                  {category.slice(0, 20)}
+                </Typography>
+              </MenuItem>
+            );
+          })}
+        </MenuList>
+      </Stack>
     </Stack>
   );
 };
