8d84f8660c2424fb8c5dc494c1f53a1df9c5ad7c
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-19 23:48:14 +0300

    add: not detail landingpage

--- src/components/app/NoteDetailLandingPage.tsx
+++ src/components/app/NoteDetailLandingPage.tsx
@@ -1,1 +1,33 @@
-const NoteDetealLanding;
+import { Box, Stack, Typography } from '@mui/material';
+import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
+
+const NoteDetailLandingPage = () => {
+  return (
+    <Stack
+      width="60vw"
+      height="95vh"
+      textAlign="center"
+      justifyContent="center"
+    >
+      <Box>
+        <InsertDriveFileOutlinedIcon
+          sx={{ color: 'white', fontSize: '8rem' }}
+        />
+        <Typography fontSize="xx-large">Select a note to view</Typography>
+        <Typography
+          fontSize="large"
+          color="lightgrey"
+          sx={{
+            mr: '20%',
+            ml: '20%',
+          }}
+        >
+          Select a note from the list on the left to view its contents, or
+          create a new note to add to your collection.
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default NoteDetailLandingPage;
