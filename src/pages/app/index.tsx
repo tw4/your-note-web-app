7140d90871d53a2a586791b4999cc819e7d6dca3
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-19 23:46:57 +0300

    update: index page

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -9,21 +9,41 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { authControl } from '@/auth';
-import { getUserData } from '@/api';
 import AppLayout from '@/layout/AppLayout';
 import AppSideBar from '@/components/app/AppSideBar';
 import NotesSide from '@/components/app/notesSide/NotesSide';
 import CreateNote from '@/components/app/CreateNote';
+import NoteDetailLandingPage from '@/components/app/NoteDetailLandingPage';
+import { Note } from '@/types';
+import { getNoteList } from '@/api';
 
 const NotesApp = () => {
   const router = useRouter();
   const [pageIsLoading, setPageIsLoading] = useState(true);
+  const [noteDetail, setNoteDetail] = useState<Note | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [categoryList, setCategoryList] = useState<string[]>([]);
+  const [noteList, setNoteList] = useState<Note[]>([]);
 
   useEffect(() => {
     authControl(localStorage.getItem('token')!).then(res => {
       res ? setPageIsLoading(false) : router.push('/login');
     });
+
+    getNoteList(localStorage.getItem('token')!).then(res => {
+      setCategoryList(res.category);
+      setNoteList(res.notes);
+    });
   }, []);
+
+  const getNoteDetail = async (note: Note) => {
+    await setNoteDetail(null);
+    await setNoteDetail(note);
+  };
+
+  const getSelectedCategory = async (category: string) => {
+    await setSelectedCategory(category);
+  };
 
   return (
     <Box>
@@ -34,9 +54,22 @@ const NotesApp = () => {
       ) : (
         <AppLayout>
           <Stack direction="row" height="95vh">
-            <AppSideBar />
-            <NotesSide />
-            <CreateNote />
+            <AppSideBar
+              categoryList={categoryList}
+              selectedCategory={selectedCategory}
+              getSelectedCategory={getSelectedCategory}
+              getNoteDetail={getNoteDetail}
+            />
+            <NotesSide
+              noteList={noteList}
+              selectedCategory={selectedCategory}
+              getNoteDetail={getNoteDetail}
+            />
+            {noteDetail !== null ? (
+              <CreateNote noteDetail={noteDetail} />
+            ) : (
+              <NoteDetailLandingPage />
+            )}
           </Stack>
         </AppLayout>
       )}
