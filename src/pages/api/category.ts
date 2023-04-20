f1376c8fa109b4e3d48c11abfecdf4d0bd6921e6
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-20 14:34:07 +0300

    add: api/category delete category

--- src/pages/api/category.ts
+++ src/pages/api/category.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
-import { UserPayload } from '@/types';
+import { Note, UserPayload } from '@/types';
 import {
   collection,
   getDocs,
@@ -54,6 +54,39 @@ const post = (req: NextApiRequest, res: NextApiResponse) => {
   });
 };
 
+const deleteCategory = (req: NextApiRequest, res: NextApiResponse) => {
+  const token = req.headers.authorization!.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET!, async (error, decoded) => {
+    if (error) {
+      res.status(401).json({ message: 'Unauthorized' });
+    } else {
+      const userPayload: UserPayload = decoded as UserPayload;
+      const q = query(
+        collection(db, 'users'),
+        where('id', '==', userPayload.id)
+      );
+      const snapShot = await getDocs(q);
+      const user = snapShot.docs[0];
+      const newNoteList = user
+        .data()
+        .notes.filter((note: Note) => note.category !== req.query.category);
+      const newCategoryList = user
+        .data()
+        .category.filter((category: string) => category !== req.query.category);
+      updateDoc(user.ref, {
+        notes: newNoteList,
+        category: newCategoryList,
+      })
+        .then(() => {
+          res.status(200).json({ message: 'ok' });
+        })
+        .catch(err => {
+          res.status(500).json({ message: err });
+        });
+    }
+  });
+};
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (
     req.method === 'GET' &&
@@ -67,6 +100,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     req.headers.authorization
   ) {
     post(req, res);
+  } else if (
+    req.method === 'DELETE' &&
+    req.headers['api-key'] === process.env.API_KEY &&
+    req.headers.authorization
+  ) {
+    deleteCategory(req, res);
   }
 };
 
