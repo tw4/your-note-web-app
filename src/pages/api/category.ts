77731e244e86dc32e494109f0fd8a4d6d67e6426
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-20 13:37:36 +0300

    add: api/category post category

--- src/pages/api/category.ts
+++ src/pages/api/category.ts
@@ -1,7 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 import { UserPayload } from '@/types';
-import { collection, getDocs, query, where } from '@firebase/firestore';
+import {
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from '@firebase/firestore';
 import { db } from '@/services/Firebase';
 
 const get = (req: NextApiRequest, res: NextApiResponse) => {
@@ -22,6 +28,32 @@ const get = (req: NextApiRequest, res: NextApiResponse) => {
   });
 };
 
+const post = (req: NextApiRequest, res: NextApiResponse) => {
+  const token = req.headers.authorization!.split(' ')[1];
+  jwt.verify(token, process.env.JWT_SECRET!, async (error, decoded) => {
+    if (error) {
+      res.status(401).json({ message: 'Unauthorized' });
+    } else {
+      const userPayload = decoded as UserPayload;
+      const q = query(
+        collection(db, 'users'),
+        where('id', '==', userPayload.id)
+      );
+      const snapShot = await getDocs(q);
+      const user = snapShot.docs[0];
+      const categoryList: string[] = user.data().category;
+      !categoryList.includes(req.body.category)
+        ? categoryList.push(req.body.category)
+        : null;
+
+      await updateDoc(user.ref, {
+        category: categoryList,
+      });
+      res.status(200).json({ message: 'ok', categoryList: categoryList });
+    }
+  });
+};
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (
     req.method === 'GET' &&
@@ -29,6 +61,12 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
     req.headers.authorization
   ) {
     get(req, res);
+  } else if (
+    req.method === 'POST' &&
+    req.headers['api-key'] === process.env.API_KEY &&
+    req.headers.authorization
+  ) {
+    post(req, res);
   }
 };
 
