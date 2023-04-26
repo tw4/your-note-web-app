8f7e6fbd20a9dde22aed8e75fe234d467ab3a1bc
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-04-26 10:44:55 +0300

    add: backdrop

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,29 @@
 import type { FC, ReactNode } from 'react';
-import { Box } from '@mui/material';
+import { Box, Backdrop, CircularProgress } from '@mui/material';
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
 
 type IProps = {
   children?: ReactNode;
+  isLogin?: boolean;
 };
 
-const Layout: FC<IProps> = ({ children }) => {
+const Layout: FC<IProps> = ({ children, isLogin = true }) => {
   return (
     <Box zIndex="1">
-      <Navbar />
-      <Box>{children}</Box>
-      <Footer />
+      {isLogin ? (
+        <>
+          <Navbar />
+          <Box>{children}</Box>
+          <Footer />
+        </>
+      ) : (
+        <>
+          <Backdrop open={!isLogin} sx={{ color: '#fff' }}>
+            <CircularProgress color="inherit" />
+          </Backdrop>
+        </>
+      )}
     </Box>
   );
 };
